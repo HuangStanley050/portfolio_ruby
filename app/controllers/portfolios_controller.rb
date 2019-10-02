073f651aab8fe15dff0e821acdd72cd2932823a5
Stanley Huang <huang.stanley050@gmail.com>
2019-10-02 15:37:52 +1000

    routes set up

---
+++ app/controllers/portfolios_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class PortfoliosController < ApplicationController
+  def index; end
+
+  def about; end
+
+  def portfolio; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'portfolios#index', as: 'root'
+  get '/about', to: 'portfolios#about', as: 'about'
+  get '/portfolio', to: 'portfolios#portfolio', as: 'portfolio'
+  get '/resume', to: 'portfolios#resume', as: 'resume'
+  get '/contact', to: 'portfolios#contact', as: 'contact'
+  get '/blogs', to: 'portfolios#blogs', as: 'blogs'
 end
